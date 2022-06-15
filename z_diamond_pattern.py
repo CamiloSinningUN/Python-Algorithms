db15533da3b4bccd915367575a1c0ae87425e83e
Camilo Sinning <camilosinning.cs@gmail.com>
2022-06-15 16:42:14 -0500

    Some changes

--- z_diamond_pattern.py
+++ z_diamond_pattern.py
@@ -1,22 +1,22 @@
 import numpy as np
 
-# Funcion recursiva rombo por capas.
-def rombo(i, j, k):
+def ramas(i,j,k):
   R[i][j] = k
-  if i == n//2 and j == n//2 : #Caso base
+  if(i == n//2 and j == n//2):
     return R
-  elif i<=v and j == v+1: #Singularidad
-    rombo(i,j-1,k+1) 
-  else: #LLamados recursivos mas pequeños9
-    if j<v or (i<v and j==v):  
-      jv=j-abs(iv-i)
-      rombo(i+1,abs(iv-(i+1))+jv,k+1)
+  elif (i>=v and j == v):
+    ramas(n-i,v,k+1)
+  else:
+    if (k % 2 == 1):
+      jv=(n-1-j)-abs(iv-i)
+      ramas(i+1,abs(iv-(i+1))+jv,k+1)
     else:
-      jv=j+abs(iv-i)
-      rombo(i-1,-abs(iv-(i-1))+jv,k+1)
-      
+      jv=(n-1-j)+abs(iv-i)
+      ramas(i,-abs(iv-(i))+jv,k+1)
+    
 
-#Lectura de tamaño de matriz
+    
+
 while True:
   n = int(input("Digita un numero entero n:"))
   if n % 2 ==1:
@@ -26,7 +26,7 @@ v = int(((n-1)/2))
 iv = v
 ne = (v+1)**2+(v)**2
 R = np.zeros((n,n))
-rombo(0,v,1)
+ramas(0,v,1)
 
 # Imprimir rombo
 print('\n'.join([
--- z_pattern.py
+++ z_pattern.py
@@ -1,44 +1,55 @@
 import numpy as np
-
-# Funcion recursiva Home.
-def Home(i, j, k):
-  H[i][j] = k
-  if k == ne:  #CASO BASE
-    return H
-  elif i < v and j == v+i:  #SINGULARIDAD 1
-    Home(i+1, v-(i+1), k + 1)
-  elif i >= v and j == nc-1:  #SINGULARIDAD 2
-    Home(i+1, 0, k + 1)
+# Funcion recursiva BigZ.
+def Big_Z(i, j, k):
+  Z[i][j] = k
+  Z[(n-1)-i][(n-1)-j] = n**2-(k-1)
+  #print(Z,'\n')
+  if i == (n-1)//2 and j == n//2:  #CASO BASE
+    return Z
+  elif i <= (n-2)//3 and j == i:  #SINGULARIDAD 3.1
+    Big_Z(i, j+1, k + 1)
+  elif i <= (n-2)//3 and j == n-1-2*i:  #SINGULARIDAD 3.2
+    Big_Z(i + 1, j - 1, k + 1)
+  elif j<=(n-3)//3 and i == n-2-2*j:  #SINGULARIDAD 3.3
+    Big_Z(i-1, j, k + 1)
   else:   #LLAMADOS RECURSIVOS MÁS PEQUEÑOS
-    Home(i,j+1,k+1)
+    if j > i and j <  n-1-2*i: #4.1 HACIA LA DERECHA
+      Big_Z(i, j+1, k + 1)
+    elif i > j and i < n-2-2*j: #4.2 4.3 HACIA ARRIBA
+      Big_Z(i - 1, j, k + 1)
+    else: # DIAGONAL SECUNDARIA DESDE N-E HACIA S-O
+      Big_Z(i+1, j-1, k + 1)
 
-      
+def rotarZ(i,j):
+  temp=Z[i][j]
+  Z[i][j]=Z[n-1-j][i]
+  Z[n-1-j][i]=Z[n-1-i][n-1-j]
+  Z[n-1-i][n-1-j]=Z[j][n-1-i]
+  Z[j][n-1-i]=temp
+
+  if(i == n//2 and j == n//2):
+    return Z
+  elif(j == n-2-i):
+    rotarZ(i+1,i+1)
+  else:
+    rotarZ(i,j+1)
+
+
+
 # Programa principal
-w=True
-while w:
-  nc = int(input('Digite número entero impar de columnas :'))
-  if nc % 2 == 1:
-    w=False 
-  else:
-    print("dijite un numero impar, por favor")
-    
-v =int((nc-1)/2) 
+n = int(input('Digite número entero: '))
+Z = np.zeros((n, n))
+Big_Z(n//3, (n-1)//3, 1)
 
-w=True
-while w:
-  nf = int(input('Digite número entero de filas superior a '+str(v+1)+":"))
-  if nf >v:
-    w=False 
-  else:
-    print("dijite un numero superior a "+v+", por favor" )
-    
-ne=v*v+nc*(nf-v)
-H = np.zeros((nf, nc))
-Home(0,v,1)
-
-# Imprimir Home
 print('\n'.join([
     ''.join(['{:4}'.format(int(item) if item != 0 else '') for item in row])
-    for row in H
+    for row in Z
 ]))
-  
+print("\n\n")
+rotarZ(0,0)
+
+# Imprimir rombo
+print('\n'.join([
+    ''.join(['{:4}'.format(int(item) if item != 0 else '') for item in row])
+    for row in Z
+]))
